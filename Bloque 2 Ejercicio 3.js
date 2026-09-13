d6e0791cbe868f01fa9271267f6ad7ce3a944987
clucasl3-hue <clucasl3@unemi.edu.ec>
2026-09-13 13:26:38 -0500

    Update and rename Bloque 2 Ejercicio 3.js to Ejercicio8.py

--- Bloque 2 Ejercicio 3.js
+++
@@ -1,7 +0,0 @@
-//Inveritr los nombres de varios productos de un arreglo
-let nombresProductos = ["Laptop", "Celular", "Tablet"];
-for (let i = 0; i < nombresProductos.length; i++) {
-    let nombreActual = nombresProductos[i];
-    let nombreInvertido = nombreActual.split("").reverse().join("");
-    console.log("Nombre de los productos invertidos: " + nombreInvertido);
-}
